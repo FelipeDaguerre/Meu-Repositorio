222219873bc5516ab3f4e45eb591d12aabe4ec8e
Felipe Daguerre <felipe499@hotmail.com>
2023-02-20 22:27:42 -0300

    Váriaveis

--- Exercícios Python/Variáveis.py
+++ Exercícios Python/Variáveis.py
@@ -1,6 +1,15 @@
 
-primeiro_nome = "Felipe"
-sobre_nome = "Daguerre"
-nome_completo = primeiro_nome +" "+ sobre_nome
+#primeiro_nome = "Felipe"
+#sobre_nome = "Daguerre"
+#nome_completo = primeiro_nome +" "+ sobre_nome
+#print ("Olá, ",nome_completo)
 
-print ("Olá, ",nome_completo)
+idade = 29  
+print('Sua idade é:', idade)
+
+altura = 178.5
+print('Sua altura é: '+str(altura)+' CM')
+
+humano = True
+
+print("Você é humano? ",humano)
