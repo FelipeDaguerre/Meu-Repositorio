47a490b4743270e31ad733dca7d077797e603eb5
Felipe Daguerre <felipe499@hotmail.com>
2023-02-21 20:31:31 -0300

    exercícios 'slicing' e 'if statements'

---
+++ Exercícios Python/cortes.py
@@ -0,0 +1,14 @@
+nome = "Felipe Daguerre"
+
+#primeiro_nome = nome[:6]
+#sobre_nome = nome [0:15:2]
+primeiro_nome = nome[0:6]
+sobre_nome = nome [7:15]
+
+print(sobre_nome)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+print(website1[7:13])
+print(website2[7:16])
