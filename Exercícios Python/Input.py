f4aac0ea63d1b269bdb008e7f913edf79384b661
Felipe Daguerre <felipe499@hotmail.com>
2023-02-21 00:16:15 -0300

    exercícios

---
+++ Exercícios Python/Input.py
@@ -0,0 +1,10 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+altura = float(input("Qual a sua altura em centrimetros? "))
+
+idade = idade + 1
+
+
+print("Muito prazer, "+nome)
+print("Sua idade é:",idade)
+print("Sua altura é de: "+str(altura)+" CM")
---
+++ Exercícios Python/TiposV.py
@@ -0,0 +1,11 @@
+x = 1
+y = 2.0
+z = "3"
+
+x = str(x)
+y = str(y)
+z = str(x)
+
+print(x)
+print(y)
+print(z*3)
