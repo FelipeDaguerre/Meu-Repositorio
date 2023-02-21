c95d5ca9e1c1a147b55bd9005dc041b6e963625b
Felipe Daguerre <felipe499@hotmail.com>
2023-02-20 22:40:26 -0300

    strings

---
+++ Exercícios Python/Strings.py
@@ -0,0 +1,12 @@
+nome = "Felipe"
+
+print(len(nome))
+print(nome.find("l"))
+print(nome.capitalize())
+print(nome.upper())
+print(nome.lower())
+print(nome.isdigit())
+print(nome.isalpha())
+print(nome.count("e"))
+print(nome.replace("e","i"))
+print(nome*3)
