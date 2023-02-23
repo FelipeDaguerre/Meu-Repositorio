52c76e94b996c22d1a97f404aa4af66270f0fffc
Felipe Daguerre <105247713+FelipeDaguerre@users.noreply.github.com>
2023-02-23 18:41:32 -0300

    Delete Exercícios Python directory

--- Exercícios Python/loops_aninhados.py
+++
@@ -1,9 +0,0 @@
-
-linhas = int(input("Quantas linhas deseja? "))
-colunas = int(input("Quantas colunas deseja? "))
-simbolo = input("Digite um simbolo: ")
-
-for i in range(linhas):
-    for j in range(colunas):
-        print(simbolo, end="")
-    print()
