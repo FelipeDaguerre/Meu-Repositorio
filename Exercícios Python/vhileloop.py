79446f598f5a93d6cb4786b7485c88fbfb4c39cb
Felipe Daguerre <105247713+FelipeDaguerre@users.noreply.github.com>
2023-02-23 11:14:52 -0300

    Delete vhileloop.py

--- Exercícios Python/vhileloop.py
+++
@@ -1,9 +0,0 @@
-#while 1==1:
-    #print("Socorro! Estou preso em um loop!")
-
-nome = ""
-#while len(nome) == 0:
-while not nome:
-    nome = input(print("Digite seu nome :"))
-else:
-    print("Olá, "+nome)
